7690e9b4ea1d8a574958cba2fc000e276acb2ce6
Diego giacomelli <giacomelli@gmail.com>
2020-01-08 13:35:49 -0300

    Added NuGet package properties

--- buildNuget.cmd
+++ buildNuget.cmd
@@ -2,4 +2,4 @@ SET PACKAGE_VERSION="0.1.0"
 
 dotnet build src -c release
 mkdir .\src\nuget
-dotnet pack src/SqlAsFile/SqlAsFile.csproj -c release --no-build --output ../nuget /p:PackageVersion=%PACKAGE_VERSION%
+dotnet pack src/SqlAsFile/SqlAsFile.csproj -c release --no-build --output src/nuget /p:PackageVersion=%PACKAGE_VERSION%
